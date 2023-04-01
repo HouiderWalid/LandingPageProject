f8d1386d68afb2006359df5ce74c40983f80e94e
Houider <houiderwalid@gmail.com>
2023-04-01 11:27:55 +0000

    with necessary packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    //'@/assets/css/main.css'
+    '@/assets/css/main.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -42,15 +42,15 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
-    //'@nuxtjs/google-fonts'
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/axios',
+    '@nuxtjs/i18n'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    //'@nuxtjs/axios',
-    //'@nuxtjs/i18n',
-    //'@nuxt/image'
+
   ],
 
   // googleFonts: {
@@ -82,12 +82,47 @@ export default {
     defaultAssets: false
   },
 
+  i18n: {
+    locales: [
+      {
+        code: 'en', iso: 'en-US', dir: 'ltr', cr: ["AS", "AI", "AG", "AU", "BS", "BB", "BZ", "BM", "BW", "IO", "CM",
+          "CA", "KY", "CX", "CC", "CK", "DM", "ER", "FK", "FJ", "GM", "GH", "GI", "GD", "GU", "GG", "GY", "HK", "IN",
+          "IE", "IM", "JM", "JE", "KE", "KI", "LS", "LR", "MW", "MT", "MH", "MU", "FM", "MS", "NA", "NR", "NZ", "NG",
+          "NU", "NF", "MP", "PK", "PW", "PG", "PH", "PN", "PR", "RW", "SH", "KN", "LC", "MF", "VC", "WS", "SC", "SL",
+          "SG", "SB", "ZA", "GS", "SD", "SZ", "TZ", "TK", "TO", "TT", "TC", "TV", "UG", "GB", "US", "VU", "VG", "VI",
+          "ZM", "ZW"]
+      },
+      {
+        code: 'fr', iso: 'en-FR', dir: 'ltr', cr: ["BE", "BJ", "BF", "BI", "CM", "CA", "CF", "TD", "KM", "CG", "CD",
+          "CI", "DJ", "GQ", "FR", "GF", "PF", "GA", "GP", "GG", "GN", "HT", "JE", "LB", "LU", "MG", "ML", "MQ", "YT",
+          "MC", "NC", "NE", "RW", "RE", "BL", "MF", "PM", "SN", "SC", "CH", "TG", "VU", "WF",]
+      },
+      {
+        code: 'es', iso: 'en-ES', dir: 'ltr', cr: ["AR", "BZ", "BO", "CL", "CO", "CR", "CU", "DO", "EC", "SV", "GQ",
+          "GU", "GT", "HN", "MX", "NI", "PA", "PY", "PE", "PR", "ES", "UY", "VE",]
+      },
+      {
+        code: 'ar', iso: 'en-EG', dir: 'rtl', cr: ["DZ", "BH", "TD", "KM", "DJ", "EG", "ER", "IQ", "IL", "JO", "KW",
+          "LB", "LY", "MR", "MA", "OM", "PS", "QA", "SA", "SO", "SD", "SY", "TN", "AE", "YE"]
+      },
+      {code: 'it', iso: 'en-IT', dir: 'ltr', cr: ["VA", "IT", "SM", "CH"]}
+    ],
+    strategy: 'no_prefix',
+    defaultLocale: 'en',
+    vueI18n: {
+      silentTranslationWarn: true,
+      silentFallbackWarn: true,
+      fallbackLocale: 'en',
+      messages: {}
+    },
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     extractCSS: {
       ignoreOrder: true
     },
-    /*optimization: {
+    optimization: {
       minimize: true,
       splitChunks: {
         chunks: 'all',
@@ -103,6 +138,6 @@ export default {
           }
         }
       }
-    }*/
+    }
   }
 }
